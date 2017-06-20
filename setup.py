aa7356a2ca6f3e6795d2d3414d112fe371cd16cd
Vedarth Kulkarni <vedarthk@vedarthz.in>
2017-06-21 01:14:09 +0530

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='async_runner',
-    version='0.1.5',
+    version='0.1.6',
     description="Async Runner",
     long_description=readme + '\n\n' + history,
     author="Vedarth Kulkarni",
